011edeaa06570073ae9ea12657d5939ae54ce18d
@Robs_dagreat <robsrwibutso12@gmail.com>
2024-03-14 21:07:23 +0200

    validated the signUp form successfully

--- scripts/signUp.js
+++ scripts/signUp.js
@@ -3,11 +3,26 @@
     document.addEventListener('DOMContentLoaded', ()=>{
 
         const signupForm = document.getElementById("signupForm");
+        const inputPass= document.getElementById('signPass');
+
+        const validate= ()=>{
+            const regex= /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[\d]).{6,}$/;
+            if(inputPass.value.length < 6){
+                alert("Please choose a password with atleast 6 characters");
+                return false;
+            } else if(!inputPass.value.match(regex)){
+               alert("Your password must contain atleast a capital letter, a number and a special character");
+               return false;
+            } 
+
+            return true;
+            
+        }
 
 
         signupForm.addEventListener('submit', (event)=>{
             event.preventDefault();
-
+            
        
             const email= event.target.elements.email.value.trim();
             const password= event.target.elements.password.value.trim();
@@ -17,7 +32,9 @@
                 return;
             }
 
-            console.log(email,password);
+            if(!validate()){
+                return;
+            }
             
             const signedUsers= JSON.parse(localStorage.getItem('users')) || [];
             const existingUser= signedUsers.some(user => user.email === email);
@@ -34,6 +51,8 @@
             signedUsers.push(newUser);
 
             localStorage.setItem('users', JSON.stringify(signedUsers));
+
+            
 
             alert("Signed up successfully");
 
